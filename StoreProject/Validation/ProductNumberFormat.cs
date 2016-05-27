54d731a28a368a91b262ce5bd6b6d14db4175d38
XyrWL <peter.trullenque@gmail.com>
2016-05-27 10:08:09 +0200

    Final commit.

--- StoreProject/Validation/ProductNumberFormat.cs
+++ StoreProject/Validation/ProductNumberFormat.cs
@@ -10,7 +10,11 @@ namespace StoreProject.Validation
     public class ProductNumberFormatAttribute : ValidationAttribute
     {
         public override bool IsValid(object value)
-            => Regex.IsMatch(value.ToString(), @"^p-\d+$");
+        {
+            if (value == null)
+                return false;
+            return Regex.IsMatch(value.ToString(), @"^p-\d+$");
+        }
 
         public override string FormatErrorMessage(string name)
             => $"{name} must be preceded by 'p-' then followed by a number.";
