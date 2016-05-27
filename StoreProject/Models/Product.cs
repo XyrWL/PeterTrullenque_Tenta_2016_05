6dd7b68d4af4d6ec68f0c64c802ca963e8adf231
XyrWL <peter.trullenque@gmail.com>
2016-05-27 09:23:19 +0200

    Exercise 1 done.

---
+++ StoreProject/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace StoreProject.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string ProductText { get; set; }
+        public int ProductNumber { get; set; }
+        public double Price { get; set; }
+        public int ProductsInStock { get; set; }
+
+        public virtual Store Store { get; set; }
+    }
+}
---
+++ StoreProject/Models/StoreContext.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace StoreProject.Models
+{
+    public class StoreContext : DbContext
+    {
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public StoreContext() : base("StoreDB")
+        {
+            Database.SetInitializer(new StoreInitializer());
+        }
+    }
+
+    public class StoreInitializer : DropCreateDatabaseAlways<StoreContext>
+    {
+        protected override void Seed(StoreContext context)
+        {
+            var stores = new List<Store>
+            {
+                new Store
+                {
+                    Name = "FirstStore",
+                    Address = "First street, 1",
+                    Products = new List<Product>
+                    {
+                        new Product
+                        {
+                            Name = "Apple",
+                            ProductText = "These are some nice apples.",
+                            ProductNumber = 5,
+                            ProductsInStock = 10,
+                            Price = 5.5
+                        },
+                        new Product
+                        {
+                            Name = "Orange",
+                            ProductText = "These are some decent oranges.",
+                            ProductNumber = 2,
+                            ProductsInStock = 12,
+                            Price = 4.9
+                        }
+                    }
+                },
+                new Store
+                {
+                    Name = "SecondStore",
+                    Address = "Second street, 2",
+                    Products = new List<Product>
+                    {
+                        new Product
+                        {
+                            Name = "Wine",
+                            ProductText = "This is a pretty good wine.",
+                            ProductNumber = 4,
+                            ProductsInStock = 20,
+                            Price = 199.9
+                        },
+                        new Product
+                        {
+                            Name = "Beer",
+                            ProductText = "This is an acceptable beer.",
+                            ProductNumber = 20,
+                            ProductsInStock = 250,
+                            Price = 9.9
+                        }
+                    }
+                }
+            };
+            context.Stores.AddRange(stores);
+            context.SaveChanges();
+        }
+    }
+}
